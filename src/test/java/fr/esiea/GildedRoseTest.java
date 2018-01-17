5414b3ef48482e4fe72ee37bb5f53579b3184dd9
kamal951 <kamal-allali@hotmail.fr>
2018-01-17 16:31:53 +0100

    Add some tests

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -8,7 +8,7 @@ public class GildedRoseTest{
 	@Test
 	public void non_specific_object_quality_and_price_decrease_by_one(){
 		Item item = new Item("apple", 2, 10);
-	Item[] items = new Item[]{item};
+		Item[] items = new Item[]{item};
 		
 		GildedRose tavern = new GildedRose(items);
 		
@@ -21,4 +21,65 @@ public class GildedRoseTest{
 		
 		softly.assertAll();
 	}
+	
+	 @Test
+    public void non_specific_object_sellin_negative() {
+        Item item = new Item("apple", 0, 10);
+		Item[] items = new Item[]{item};
+		
+		GildedRose tavern = new GildedRose(items);
+		
+		tavern.updateQuality();
+		
+		SoftAssertions softly = new SoftAssertions();
+		softly.assertThat(item.quality).as("Apple quality").isEqualTo(8);
+		
+		softly.assertThat(item.sellIn).as("Apple price").isEqualTo(-1);
+		
+		softly.assertAll();
+    }
+	
+	@Test
+    public void aged_brie_quality_and_sellin() {
+        Item item = new Item("Aged Brie", 0, 10);
+        Item[] items = new Item[]{item};
+
+        GildedRose tavern = new GildedRose(items);
+		
+        tavern.updateQuality();
+
+        SoftAssertions solftly = new SoftAssertions();
+        solftly.assertThat(item.quality).as("Aged Brie quality").isEqualTo(12);
+
+        solftly.assertThat(item.sellIn).as("Aged Brie price").isEqualTo(-1);
+
+        solftly.assertAll();
+    }
+
+    @Test
+    public void tafkal80etc_quality_and_sellin() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+        Item[] items = new Item[]{item};
+
+        GildedRose tavern = new GildedRose(items);
+		
+        tavern.updateQuality();
+
+        SoftAssertions solftly = new SoftAssertions();
+        solftly.assertThat(item.quality).as("TAFKAL80ETC quality").isEqualTo(0);
+
+        solftly.assertThat(item.sellIn).as("TAFKAL80ETC price").isEqualTo(-1);
+
+        solftly.assertAll();
+    }
+
+    @Test
+    public void non_specific_object_to_string() {
+        Item item = new Item("Apple", 10, 10);
+        SoftAssertions solftly = new SoftAssertions();
+
+        solftly.assertThat(item.toString()).as("Apple toString method").isEqualTo("Apple, 10, 10");
+
+        solftly.assertAll();
+    }
 }
