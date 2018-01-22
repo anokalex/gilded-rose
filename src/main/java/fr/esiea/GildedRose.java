48c023fbed1de78cb4b1cf59ef3c8a421ab92150
Snadjim <nadjimsadji@gmail.com>
2018-01-22 15:57:29 +0100

    Update GildedRose.java

--- src/main/java/fr/esiea/GildedRose.java
+++ src/main/java/fr/esiea/GildedRose.java
@@ -10,15 +10,12 @@ class GildedRose {
     //Methode qui update pour Aged Brie
     private void updateAgedBrie(Item i){
         if(i.getQuality() < 50){
-           // i.incQuality();
             i.setQuality(i.getQuality() + 1);
         }
 
         i.setSellIn(i.getSellIn() - 1);
-        //i.decSellIn();
 
         if(i.getSellIn() < 0 && i.getQuality() < 50){
-            //i.incQuality();
             i.setQuality(i.getQuality() + 1);
         }
     }
@@ -26,21 +23,15 @@ class GildedRose {
     //Methode qui update pour TAFKAL80ETC
     private void updateTAFKAL80ETC(Item i){
         if(i.getQuality() < 50){
-            //i.incQuality();
             i.setQuality(i.getQuality() + 1);
             if(i.getSellIn() < 11){
                 i.setQuality(i.getQuality() + 1);
-                //i.incQuality();
             }
             if(i.getSellIn() < 6){
                 i.setQuality(i.getQuality() + 1);
-//                i.incQuality();
             }
         }
-
-//        i.decSellIn();
         i.setSellIn(i.getSellIn() - 1);
-
         if(i.getSellIn() < 0){
             i.setQuality(0);
         }
@@ -50,17 +41,15 @@ class GildedRose {
     private void updateItem(Item i){
         if(i.getQuality() > 0){
             i.setQuality(i.getQuality() - 1);
-//            i.decQuality();
         }
 
-//        i.decSellIn();
         i.setSellIn(i.getSellIn() - 1);
         if(i.getSellIn() < 0 && i.getQuality() > 0){
-//            i.decQuality();
             i.setQuality(i.getQuality() - 1);
         }
     }
 
+    //Méthode pour update un item en fonction de son nom
     public Item update(Item i){
         if(i.getName().equals("Aged Brie")){
             this.updateAgedBrie(i);
